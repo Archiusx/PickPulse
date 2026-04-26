b3000e0882454173ff8729033c5d8d467cecc040
Piyush Rajesh Deshkar <shacx8@gmail.com>
2026-04-26 11:47:36 +0530

    feat(firebase): Embed firebase config
    Embed Firebase configuration directly into the `firebase.ts` file instead of importing it from a JSON file. This simplifies the build process and reduces external dependencies for configuration.

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { initializeFirestore } from 'firebase/firestore';
-// @ts-ignore
-import firebaseConfig from './firebase-applet-config.json';
+const firebaseConfig = {
+  apiKey: "AIzaSyDAwty9lfGsj-hAf1QffMJLtvUfhdd_SPI",
+  authDomain: "loginx-897b3.firebaseapp.com",
+  databaseURL: "https://loginx-897b3-default-rtdb.firebaseio.com",
+  projectId: "loginx-897b3",
+  storageBucket: "loginx-897b3.firebasestorage.app",
+  messagingSenderId: "380291415413",
+  appId: "1:380291415413:web:6d222db905e4457e29e73c",
+  measurementId: "G-BM6PMDKN5V"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
-}, firebaseConfig.firestoreDatabaseId);
+});
 export const auth = getAuth(app);
